c98bb6dfdb6babf00b78abc86cab06ec42ec2e39
Shivansh Saini <shivanshs9@gmail.com>
2020-08-24 15:03:05 +0530

    refactor: initdb wrapper to save plugin load time

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,10 @@
+from functools import wraps
+
+from airflow.utils import db
 from airflow.utils.state import State
 
 SECTION_NAME = "ergo"
+
 
 class JobResultStatus(object):
     NONE = 0
@@ -14,3 +18,23 @@ class JobResultStatus(object):
             return State.QUEUED
         else:
             return State.FAILED
+
+
+def ergo_initdb(func):
+    from ergo.migrations.utils import initdb
+
+    prev_wrappers = getattr(func, '_wrappers', list())
+    if SECTION_NAME in prev_wrappers:
+        return func
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        initdb()
+
+    wrapper._wrappers = list(prev_wrappers) + list(SECTION_NAME)
+
+    return wrapper
+
+
+db.initdb = ergo_initdb(db.initdb)
--- src/plugin.py
+++ src/plugin.py
@@ -9,23 +9,9 @@ from ergo.migrations.utils import initdb
 from ergo.operators.task_producer import ErgoTaskProducerOperator
 from ergo.sensors.job_result_sensor import ErgoJobResultSensor
 
-DAG_FOLDER = path.join(path.dirname(__file__), 'dags')
-TAG = 'ergo'
-
 
 class ErgoPlugin(AirflowPlugin, LoggingMixin):
     name = 'ergo'
     operators = (ErgoTaskProducerOperator,)
     sensors = (ErgoJobResultSensor,)
     log = logging.root.getChild(f'{__name__}.{"ErgoPlugin"}')
-
-    @classmethod
-    def validate(cls):
-        super().validate()
-        initdb()
-        # FIXME: Hack since on_load calls only for entrypoint plugins
-        # cls.log.info('%s: Loading DAGS from %s...', TAG, DAG_FOLDER)
-        # dag_bag = DagBag(dag_folder=DAG_FOLDER, include_examples=False)
-        # for dag_id, dag in dag_bag.dags.items():
-        #     cls.log.info('%s: Found dag %s', TAG, dag_id)
-        #     globals()[dag_id] = dag
