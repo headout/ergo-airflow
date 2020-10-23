0befb30e61d014d00721bbac1fa3a4f6563468ef
Shivansh Saini <shivanshs9@gmail.com>
2020-10-23 12:12:14 +0530

    feat: job result sensor compatible with smart sensor

--- src/sensors/job_result_sensor.py
+++ src/sensors/job_result_sensor.py
@@ -1,14 +1,17 @@
+from datetime import datetime
+
 from airflow.sensors.base_sensor_operator import BaseSensorOperator
 from airflow.utils.db import provide_session
 from airflow.utils.decorators import apply_defaults
 from airflow.utils.state import State
-from sqlalchemy.orm import joinedload
-
 from ergo.exceptions import ErgoFailedResultException
 from ergo.models import ErgoJob, ErgoTask
+from sqlalchemy.orm import joinedload
 
 
 class ErgoJobResultSensor(BaseSensorOperator):
+    poke_context_fields = ('pusher_task_id', 'wait_for_state')
+
     @apply_defaults
     def __init__(
         self,
@@ -17,6 +20,9 @@ class ErgoJobResultSensor(BaseSensorOperator):
         *args,
         **kwargs
     ):
+        # HACK: In Airflow V2, for smart sensor, added 'ti' info in context but
+        # it shouldn't be passed to super constructor
+        kwargs.pop('ti_dict')
         super().__init__(*args, **kwargs)
         self.pusher_task_id = pusher_task_id
         if not isinstance(wait_for_state, (list, tuple)):
@@ -26,16 +32,31 @@ class ErgoJobResultSensor(BaseSensorOperator):
             self.wait_for_state.extend(wait_for_state)
 
     @provide_session
-    def _get_ergo_task(self, ti, session=None):
+    def _get_ergo_task(self, ti_dict, session=None):
         return (
             session.query(ErgoTask)
             .options(joinedload('job'))
-            .filter_by(ti_task_id=self.pusher_task_id, ti_dag_id=ti.dag_id, ti_execution_date=ti.execution_date)
+            .filter_by(ti_task_id=self.pusher_task_id, ti_dag_id=ti_dict['dag_id'], ti_execution_date=ti_dict['execution_date'])
         ).one()
 
+    def get_poke_context(self, context):
+        result = super().get_poke_context(context)
+        result['ti_dict'] = {
+            'dag_id': context['ti'].dag_id,
+            'execution_date': context['ti'].execution_date.isoformat()
+        }
+        return result
+
     def poke(self, context):
-        ti = context['ti']
-        task = self._get_ergo_task(ti)
+        ti_dict = context.get('ti_dict', dict())
+        if not ti_dict:
+            ti = context['ti']
+            ti_dict['dag_id'] = ti.dag_id
+            ti_dict['execution_date'] = ti.execution_date
+        else:
+            ti_dict['execution_date'] = datetime.fromisoformat(
+                ti_dict['execution_date'])
+        task = self._get_ergo_task(ti_dict)
         self.log.info('Received task - %s... STATE: %s', str(task), task.state)
         job = task.job
         if job is not None:
