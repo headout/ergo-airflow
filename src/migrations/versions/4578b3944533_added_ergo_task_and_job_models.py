376c1ec250fc3f9b4154f92c5b07c6272123b2ab
joelkbiju12 <joel.biju@headout.com>
2023-06-02 10:00:30 +0530

    Removing ti_Execution key

--- src/migrations/versions/4578b3944533_added_ergo_task_and_job_models.py
+++ src/migrations/versions/4578b3944533_added_ergo_task_and_job_models.py
@@ -35,7 +35,7 @@ def upgrade():
     sa.Column('ti_task_id', sa.String(length=250), nullable=False),
     sa.Column('ti_dag_id', sa.String(length=250), nullable=False),
     sa.Column('ti_execution_date', col_ti_execution_date_type, nullable=False),
-    sa.ForeignKeyConstraint(['ti_task_id', 'ti_dag_id', 'ti_execution_date'], ['task_instance.task_id', 'task_instance.dag_id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['ti_task_id', 'ti_dag_id'], ['task_instance.task_id', 'task_instance.dag_id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_ergo_task_created_at'), 'ergo_task', ['created_at'], unique=False)
