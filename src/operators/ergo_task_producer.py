c5d676ee475154faf4d0b73a05ddba7714460df1
Joel K Biju <joel.biju@headout.com>
2023-08-11 00:34:41 +0530

    Adding single task

--- src/operators/ergo_task_producer.py
+++ src/operators/ergo_task_producer.py
@@ -52,41 +52,30 @@ class ErgoTaskQueuerOperator(BaseOperator):
             req_data = ''
         if not isinstance(req_data, str):
             req_data = json.dumps(req_data)
+
         self.log.info("Adding task '%s' with data: %s", task_id, req_data)
         task = ErgoTask(task_id, ti, self.ergo_task_sqs_queue_url, req_data)
         session.add(task)
-        success_resps, failed_resps = self._send_to_sqs(self.ergo_task_sqs_queue_url, task)
-        if success_resps:
-            self.log.info(
-                'Successfully pushed %d messages!', len(success_resps))
-            self._set_task_states(
-                tasks,
-                [int(resp['Id']) for resp in success_resps],
-                State.QUEUED
-            )
-            jobs = [
-                ErgoJob(resp['MessageId'], int(resp['Id']))
-                for resp in success_resps
-            ]
-            session.add_all(jobs)
-        if failed_resps:
-            self.log.error('Failed to push %d messages!', len(failed_resps))
+        success_resp, failed_resp = self._send_to_sqs(self.ergo_task_sqs_queue_url, task)
 
+        if success_resp:
+            self.log.info('Successfully pushed SQS task request message')
+            self._set_task_states(task,[int(resp['Id']) for resp in success_resp],State.QUEUED)
+            jobs = [ErgoJob(resp['MessageId'], int(resp['Id'])) for resp in success_resp ]
+            session.add_all(jobs)
+
+        if failed_resp:
+            self.log.info('Failed pushed SQS task request message')
             self.log.info("Setting the tasks up for reschedule!")
-            self._set_task_states(
-                tasks,
-                [int(resp['Id']) for resp in failed_resps],
-                State.UP_FOR_RESCHEDULE,
-            )
-        session.commit()
+            self._set_task_states(task,[int(resp['Id']) for resp in failed_resp],State.UP_FOR_RESCHEDULE)
+
+
+    session.commit()
         self.log.info("Commited task '%s' to %s", str(task), task.state)
 
-    def _send_to_sqs(self, queue_url, tasks) -> Tuple[List, List]:
+    def _send_to_sqs(self, queue_url, task) -> Tuple[List, List]:
         sqs_client = SQSHook(aws_conn_id=self.aws_conn_id).get_conn()
-        self.log.info('Trying to push %d messages on queue: %s\n',
-                      len(tasks), queue_url)
-        self.log.info('Request tasks: ' + '\n'.join([str(task) for task in tasks]))
-
+        self.log.info('Trying to push %s messages on queue: %s\n',task.task_id, queue_url)
         entries = [
             {
                 'Id': str(task.id),
@@ -94,24 +83,25 @@ class ErgoTaskQueuerOperator(BaseOperator):
                 'MessageGroupId': task.task_id,
                 'MessageDeduplicationId': str(task.id)
             }
-            for task in tasks
         ]
         try:
             response = sqs_client.send_message_batch(
                 QueueUrl=queue_url,
                 Entries=entries
             )
-            success_resps = response.get('Successful', list())
-            failed_resps = response.get('Failed', list())
+            succes_resp = response.get('Successful', list())
+            failed_resp = response.get('Failed', list())
         except Exception as e:
             self.log.exception(
                 'SQS Send message API failed for "%s" queue!\nRequest Entries: %', queue_url, str(
                     entries),
                 exc_info=e
             )
-            success_resps = list()
-            failed_resps = list(entries)
-        return (success_resps, failed_resps)
+            success_resp = list()
+            failed_resp = list(entries)
+
+        return success_resp,failed_resp
+
 
     @staticmethod
     def _set_task_states(tasks, task_ids, state):
