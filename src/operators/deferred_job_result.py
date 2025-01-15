912fc2af351bc36c3c4d40d2f5ffe4f5ace27fc4
Joel K Biju <joel.biju@headout.com>
2025-01-15 23:53:19 +0530

    fixing

--- src/operators/deferred_job_result.py
+++ src/operators/deferred_job_result.py
@@ -19,6 +19,7 @@ class ErgoDeferredJobResult(BaseOperator):
             self,
             pusher_task_id: str,
             wait_for_state=list(State.finished),
+            task_poll_trigger=True,
             *args,
             **kwargs
     ):
@@ -27,11 +28,13 @@ class ErgoDeferredJobResult(BaseOperator):
         kwargs.pop('ti_dict', None)
         super().__init__(*args, **kwargs)
         self.pusher_task_id = pusher_task_id
+        self.task_poll_trigger = task_poll_trigger
         if not isinstance(wait_for_state, (list, tuple)):
             wait_for_state = (wait_for_state,)
         self.wait_for_state = list(State.finished)
         if self.wait_for_state != wait_for_state:
             self.wait_for_state.extend(wait_for_state)
+
 
     def _get_ergo_task(self, ti_dict, session=None):
         return (
@@ -57,6 +60,9 @@ class ErgoDeferredJobResult(BaseOperator):
                 self.log.info('Waiting for task "%s" to be queued...', str(task))
                 self.log.info('Waiting for task "%s" to reach state %s...', str(task), self.wait_for_state)
 
+            if self.task_poll_trigger:
+                self.defer(trigger=TimeDeltaTrigger(timedelta(seconds=20)), method_name="execute_complete")
+
         if task.state == State.FAILED:
             if job is not None:
                 self.log.info('Job - (%s)' + (f'responded back at {job.response_at}' if job.response_at else ''), str(job))
@@ -69,12 +75,17 @@ class ErgoDeferredJobResult(BaseOperator):
 
     def execute(self, context, event=None):
         ti_dict = context.get('ti_dict', dict())
+        self.xcom_push(context, "ergo_task_state", "started")
         if not ti_dict:
             ti = context['ti']
             ti_dict['dag_id'] = ti.dag_id
             ti_dict['run_id'] = ti.run_id
-        self.log.info('Polling DB task status using task poll trigger. Check triggerer logs to get more state info')
-        self.defer(trigger=TaskPollTrigger(ti_dict, self.pusher_task_id, self.wait_for_state, 20), method_name="execute_complete")
+        if self.task_poll_trigger:
+            self.log.info('Polling DB task status using task poll trigger. Check triggerer logs to get more state info')
+            self.defer(trigger=TaskPollTrigger(ti_dict, self.pusher_task_id, self.wait_for_state, 20), method_name="execute_complete")
+        else:
+            self.log.info('Polling DB task status using airflow worker and timedelta trigger')
+            self.defer(trigger=TimeDeltaTrigger(timedelta(seconds=20)), method_name="execute_complete")
         return
 
     def execute_complete(self, context, event=None):
@@ -85,6 +96,7 @@ class ErgoDeferredJobResult(BaseOperator):
             ti_dict['run_id'] = ti.run_id
         self.log.info("Control transferred from trigger to worker for remaining operator execution")
         self._get_task_status(ti_dict)
+        self.xcom_push(context, "ergo_task_state", "completed")
         return
 
 
